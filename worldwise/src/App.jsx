28ec3fb47801ab1b2009e0bc6c00efab6d2e6bf7
byunn90 <kayhanturk90@Live.com>
2024-06-26 22:23:05 +1000

    sidebar not working

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -4,15 +4,22 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="product" element={<Product />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
   );
--- worldwise/src/components/PageNav.jsx
+++ worldwise/src/components/PageNav.jsx
@@ -1,17 +1,24 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink to="/HomePage">Home</NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/Product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
