e475e6ad53139af744cda17d45aca3a893902ef8
byunn90 <kayhanturk90@Live.com>
2024-07-18 12:00:19 +1000

    children

--- worldwise/src/components/Sidebar.jsx
+++ worldwise/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
+import { Children } from "react";
 
 function Sidebar() {
   return (
@@ -10,7 +11,7 @@ function Sidebar() {
       <AppNav />
 
       <Outlet />
-
+      {Children}
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
